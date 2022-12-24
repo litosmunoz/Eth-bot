f4494f2e98c38bed188f18e5c15b254828b248d7
Carlos <carlos.mfresco@gmail.com>
2022-12-24 11:17:48 +0100

    up

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def strategy_short(qty, open_position = False):
                             
         df = get5minutedata()
         apply_technicals(df)
-        print(f"Buyprice: {buyprice}" + '             Close: ' + str(df.Close.iloc[-1]))
+        print(f"Buyprice: {buyprice}" + '              Close: ' + str(df.Close.iloc[-1]))
         print(f'Target: ' + str(round(buyprice * 0.93, 2)) + "               Stop: " + str(round(buyprice * 1.03, 2)))
         print(f'RSI Target: 25' + '                RSI: ' + str(df.RSI.iloc[-1]))
         print("---------------------------------------------------")
