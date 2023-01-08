924dfa85b8a757af33208db2506ed2b051d18199
Carlos <carlos.mfresco@gmail.com>
2023-01-08 17:23:27 +0100

    up

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ session = usdt_perpetual.HTTP(
 # In[5]:
 
 
-#This function gets Real SOL Price Data and creates a smooth dataframe that refreshes every 15 minutes
+#This function gets Real ETH Price Data and creates a smooth dataframe that refreshes every 5 minutes
 def get5minutedata():
     frame = pd.DataFrame(session_auth.query_kline(symbol="ETHUSDT", interval="5m")["result"])
     frame = frame.iloc[:,: 6]
